6e7098a9d6f8769f702c4dceb5cb5f2cab54959e
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2021-07-03 13:45:42 +0530

    changed innertext to innerhtml for display

--- Falchion-Chromium/src/options/options.js
+++ Falchion-Chromium/src/options/options.js
@@ -3,14 +3,14 @@ const params = new URLSearchParams(window.location.search);
 if (params.get("method").toUpperCase() == "GET") {
     fetch(params.get("url"), {
         method: params.get("method"),
-        headers: JSON.parse(params.get("headers"))
-    }).then(res => res.text().then(e => document.body.innerText = e)).catch(err => document.body.innerText = err)
+        headers: JSON.parse(params.get("headers")),
+    }).then(res => res.text().then(e => document.body.innerHTML = e)).catch(err => document.body.innerText = err)
 }
 else {
     fetch(params.get("url"), {
         body: JSON.parse(params.get("body")),
         method: params.get("method"),
-        headers: JSON.parse(params.get("headers"))
-    }).then(res => res.json().then(e => document.body.innerText = e)).catch(err => document.body.innerText = err)
+        headers: JSON.parse(params.get("headers")),
+    }).then(res => res.json().then(e => document.body.innerHTML = e)).catch(err => document.body.innerText = err)
 }
 
